8f49b4770ed0e33bbec739488923dd12f1257688
jragresta <jragresta@gmail.com>
2021-09-25 20:22:56 -0400

    commit

--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -12,10 +12,10 @@ function Section2(){
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia lacus id dolor dictum volutpat. Donec id lorem nec lorem facilisis tempus. In commodo sapien sed nulla mattis, in auctor dolor varius. Vivamus dignissim ac tortor commodo fringilla. In cursus est eget lacus scelerisque congue. Aenean vel finibus nulla. Etiam suscipit metus non gravida venenatis. Quisque vitae volutpat lorem. Nullam aliquam ante nisi, vel egestas erat bibendum a. Nam eu nulla nisl.
                     </p>
                 </div>
+            </div>
                 <div className='image-astro'>
                     <img src={astro} alt='astro'/> 
                 </div>
-            </div>
         </div>
         </>
     );
